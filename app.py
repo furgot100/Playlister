86429b540c191f2326cf12d8076dfefdc5d0712c
Francis Tsang <frtsang40@gmail.com>
2019-10-01 12:47:44 -0700

    add created_at, footer and theme

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+from datetime import datetime
 import os
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
@@ -27,9 +28,10 @@ def playlists_submit():
     playlist = {
         'title' : request.form.get('title'),
         'description' : request.form.get('description'),
-        'videos' : request.form.get('videos').split()
+        'videos' : request.form.get('videos').split(),
+        'created_at': datetime.now()
     }
-    #playlists.insert_one(playlist)
+    print(playlist)
     playlist_id = playlists.insert_one(playlist).inserted_id
     return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
@@ -75,5 +77,10 @@ def comments_new():
     comment_id = comments.insert_one(comment).inserted_id
     return redirect(url_for('playlists_show', playlist_id=request.form.get('playlist_id')))
 
+@app.route('/playlists/comments/<comment_id>', methods=['POST'])
+def comments_delete(comment_id):
+    comment = comments.find_one({'_id': ObjectId(comment_id)})
+    comments.delete_one({'_id': ObjectId(comment_id)})
+    return redirect(url_for('playlists_show', playlist_id=comment.get('playlist_id')))
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
